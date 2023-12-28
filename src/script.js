fcd4fb390d953f92b10cd37b33cee12f9b051a1e
Motlakz <motlalepulasello5@gmail.com>
2023-12-28 23:14:12 +0200

    add more interactive features

--- src/script.js
+++ src/script.js
@@ -13,11 +13,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
       // Function to display a quiz
       function displayQuiz(quiz) {
-        const quizTopicElement = document.querySelector('.topic-name');
-        const quizIconElement = document.querySelector('.quiz-icon');
+        const quizTopicElement = document.querySelector('.quiz-topic');
 
-        quizTopicElement.innerText = quiz.title;
-        quizIconElement.innerHTML = `<i class="${quiz.icon}"></i>`;
+        quizTopicElement.innerHTML = `
+          <h2 class="topic-name font-semibold flex items-center"><img src=${quiz.icon} alt="quiz icon"> ${quiz.title}</h2>        
+        `;
 
         const questionNumberElement = document.getElementById('question-number');
         const remainingQuestionsElement = document.getElementById('remaining-questions');
@@ -26,6 +26,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // Display the first question initially
         let currentQuestionIndex = 0;
+
+        const progressValue = (currentQuestionIndex / quiz.questions.length) * 100;
+        const progressBar = document.querySelector('.progress');
+        progressBar.style.width = `${progressValue}%`;
+
+        function updateProgressBar() {
+          const progressValue = (currentQuestionIndex / quiz.questions.length) * 100;
+          progressBar.style.width = `${progressValue}%`;
+        }
+
         displayQuestion(quiz.questions[currentQuestionIndex]);
 
         function displayQuestion(question) {
@@ -42,44 +52,66 @@ document.addEventListener('DOMContentLoaded', function () {
           question.options.forEach((option, index) => {
             const optionLabel = String.fromCharCode(65 + index);
         
-            // Convert HTML string to text content
-            const optionTextNode = document.createTextNode(option);
+            // Function to escape HTML special characters so code format questions can be rendered on the page
+            const escapeHTML = (str) => {
+                const div = document.createElement('div');
+                div.appendChild(document.createTextNode(str));
+                return div.innerHTML;
+            };
         
-            const optionElement = document.createElement('label');
-            optionElement.classList.add('block', 'text-lg', 'mb-2', 'font-bold', 'cursor-pointer', 'group');
-        
-            const radioInput = document.createElement('input');
-            radioInput.classList.add('hidden');
-            radioInput.type = 'radio';
-            radioInput.name = 'answer';
-            radioInput.value = option;
-        
-            const optionContent = document.createElement('p');
-            const spanElement = document.createElement('span');
-            spanElement.classList.add('p-3', 'group-hover:bg-pink-500', 'dark:group-hover:bg-pink-300', 'mr-3', 'border', 'border-gray-500', 'dark:border-none', 'rounded-md', 'bg-[#F4F6FA]', 'text-gray-600', 'group-hover:text-white');
-            spanElement.appendChild(document.createTextNode(optionLabel));
-            optionContent.appendChild(spanElement);
-            optionContent.classList.add('p-4', 'pl-2', 'group', 'border-2', 'dark:hover:border-pink-300', 'hover:border-pink-600', 'border-transparent', 'rounded-md', 'bg-pink-100', 'shadow-lg', 'text-gray-800', 'dark:text-white', 'dark:shadow-purple-700/50', 'm-1', 'dark:bg-purple-600');
-            optionContent.appendChild(optionTextNode);
-        
-            optionElement.appendChild(radioInput);
-            optionElement.appendChild(optionContent);
-        
-            // Add click event listener to each option
-            optionElement.addEventListener('click', function(event) {
-              const selectedAnswer = option;
-              const correctAnswer = quiz.questions[currentQuestionIndex].answer;
+            // HTML representation of the structure for each created element
+            const optionHTML = `
+              <label class="block text-lg mb-2 font-bold cursor-pointer your-option-class" onclick="checkAnswer(event, '${option}')">
+                <input class="hidden" type="radio" name="answer" value="${option}">
+                <p class="p-4 pl-2 border-2 border-transparent rounded-md bg-pink-100 shadow-lg text-gray-800 dark:text-white dark:shadow-purple-700/50 m-1 dark:bg-purple-600">
+                  <span class="p-3 mr-3 border border-gray-500 dark:border-none rounded-md bg-[#F4F6FA] text-gray-600">${optionLabel}</span>
+                  ${escapeHTML(option)}
+                </p>
+              </label>
+            `;
 
-              if (selectedAnswer === correctAnswer) {
-                optionContent.classList.add('correct');
-              } else {
-                optionContent.classList.add('incorrect')
+            // Convert the HTML string to a DOM element
+            const optionElement = new DOMParser().parseFromString(optionHTML, 'text/html').body.firstChild;
+
+            // Add click event listener to each option
+            answerOptionsElement.addEventListener('click', function (event) {
+              const targetLabel = event.target.closest('label');
+
+              if (targetLabel) {
+                const selectedAnswer = targetLabel.querySelector('input').value;
+                const correctAnswer = quiz.questions[currentQuestionIndex].answer;
+
+                const optionParagraph = targetLabel.querySelector('p');
+
+                if (selectedAnswer === correctAnswer) {
+                  targetLabel.classList.add('correct');
+                  targetLabel.classList.remove('incorrect');
+                  optionParagraph.classList.add('correct');
+                  optionParagraph.classList.remove('incorrect');
+                  optionParagraph.style.border = '2px solid var(--correct-border-color)'
+
+                } else {
+                  targetLabel.classList.add('incorrect');
+                  targetLabel.classList.remove('correct');
+                  optionParagraph.classList.add('incorrect');
+                  optionParagraph.classList.remove('correct');
+                  optionParagraph.style.border = '2px solid var(--incorrect-border-color)';
+                }
+
+                // Remove the 'correct' or 'incorrect' class from all labels
+                const allLabels = answerOptionsElement.querySelectorAll('label');
+                allLabels.forEach((label) => {
+                  if (label !== targetLabel) {
+                    label.classList.remove('correct', 'incorrect');
+                  }
+                });
+
+                // Prevent the form from being submitted when an option is clicked
+                event.preventDefault();
               }
-        
-              // Prevent the form from being submitted when an option is clicked
-              event.preventDefault();
             });
-        
+
+            // Append the option to the container in the DOM
             answerOptionsElement.appendChild(optionElement);
           });
 
@@ -90,10 +122,11 @@ document.addEventListener('DOMContentLoaded', function () {
           submitButton.addEventListener('click', function(event) {
             // Move to the next question or finish the quiz
             currentQuestionIndex++;
+            updateProgressBar();
             if (currentQuestionIndex < quiz.questions.length) {
               displayQuestion(quiz.questions[currentQuestionIndex]);
             } else {
-              console.log('Quiz Finished');
+              alert('Quiz Finished');
             }
 
             event.preventDefault();
